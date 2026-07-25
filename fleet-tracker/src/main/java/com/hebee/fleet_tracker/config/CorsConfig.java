a62e8172233950c5313932de587cbee2eaf4f266
hebee-77 <hebeectr2004@gmail.com>
2026-07-25 10:52:40 +0530

    feat: update CORS configuration to allow production URL

--- fleet-tracker/src/main/java/com/hebee/fleet_tracker/config/CorsConfig.java
+++ fleet-tracker/src/main/java/com/hebee/fleet_tracker/config/CorsConfig.java
@@ -16,15 +16,16 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "http://localhost:5174",
-                                "http://localhost:5175",
-                                "http://localhost:5176"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+            	registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "http://localhost:5175",
+                        "http://localhost:5176",
+                        "https://fleet-tracker-ten.vercel.app"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*");
             }
         };
     }
--- fleet-tracker/src/main/java/com/hebee/fleet_tracker/security/config/SecurityConfig.java
+++ fleet-tracker/src/main/java/com/hebee/fleet_tracker/security/config/SecurityConfig.java
@@ -65,7 +65,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://fleet-tracker-ten.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
